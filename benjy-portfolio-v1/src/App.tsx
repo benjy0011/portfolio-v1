ea46fe893c3af4108a78189e624cac2d33f7c180
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2025-09-05 16:59:57 +0800

    feat: project init

---
+++ benjy-portfolio-v1/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <div className="w-[100vw] h-[100vh]">
+      HI IAM BENJY
+    
+      <p className="font-turret-road">
+        Turret Road
+      </p>
+    </div>
+  )
+}
+
+export default App
