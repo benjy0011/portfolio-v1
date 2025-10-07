785cbf3d1c30dfcc318c32a761ff139c176d66db
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2025-10-07 21:51:52 +0800

    chore: Minor update

--- src/features/Home.tsx
+++ src/features/Home.tsx
@@ -74,7 +74,7 @@ export default function Home () {
                   staggerDuration={0.025}
                   splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
                   transition={{ type: "spring", damping: 30, stiffness: 400 }}
-                  rotationInterval={3000}
+                  rotationInterval={1000}
                 />  
               </span> 
               <p>developer</p>
